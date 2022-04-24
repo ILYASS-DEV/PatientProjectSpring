c48d51d54849e8fa80afed06d01393ef9f219e6b
Ilyass Aidi <74270120+ILYASS-DEV@users.noreply.github.com>
2022-04-24 01:39:14 +0100

    Commit d'application

--- src/main/java/com/ilyassaidi/patientprojectspring/PatientProjectSpringApplication.java
+++ src/main/java/com/ilyassaidi/patientprojectspring/PatientProjectSpringApplication.java
@@ -5,6 +5,7 @@ import com.ilyassaidi.patientprojectspring.repositories.PatientRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import java.util.Date;
 
@@ -14,23 +15,17 @@ public class PatientProjectSpringApplication {
     public static void main(String[] args) {
         SpringApplication.run(PatientProjectSpringApplication.class, args);
     }
-
-    CommandLineRunner commandLineRunner(PatientRepository patientRepository) {
-        return args -> {
-
-            patientRepository.save(
-                    new Patient(null, "Ilyass", new Date(), false, 16));
-            patientRepository.save(
-                    new Patient(null, "Youssef", new Date(), true, 100));
-            patientRepository.save(
-                    new Patient(null, "Ali", new Date(), true, 156));
-            patientRepository.save(
-                    new Patient(null, "Sara", new Date(), false, 20));
-
+    @Bean
+    CommandLineRunner commandLineRunner(PatientRepository patientRepository){
+        return args ->{
+            patientRepository.save(new Patient(null,"Ilyass",new Date(),false,102));
+            patientRepository.save(new Patient(null,"Youssef",new Date(),false,516));
+            patientRepository.save(new Patient(null,"Kawtar",new Date(),true,433));
+            patientRepository.save(new Patient(null,"Chaimaa",new Date(),false,10));
             patientRepository.findAll().forEach(p->{
                 System.out.println(p.getNom());
             });
-        };
 
+        };
     }
 }
