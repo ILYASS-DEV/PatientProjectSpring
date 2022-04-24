2afbe89045235014f64ea0b353d9d67328d1eff3
Ilyass Aidi <74270120+ILYASS-DEV@users.noreply.github.com>
2022-04-24 18:45:11 +0100

    Edit and add patient part2

--- src/main/java/com/ilyassaidi/patientprojectspring/entities/Patient.java
+++ src/main/java/com/ilyassaidi/patientprojectspring/entities/Patient.java
@@ -3,22 +3,27 @@ package com.ilyassaidi.patientprojectspring.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.util.Date;
 @Entity
 @Data @AllArgsConstructor @NoArgsConstructor
 
 public class Patient {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+    @NotEmpty
+    @Size(min=4,max=40)
     private String nom;
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date dateNaissance;
     private boolean malade;
+    @DecimalMin("100")
     private int score;
 
 }
--- src/main/java/com/ilyassaidi/patientprojectspring/web/PatientController.java
+++ src/main/java/com/ilyassaidi/patientprojectspring/web/PatientController.java
@@ -7,10 +7,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -48,5 +51,28 @@ public class PatientController {
         return patientRepository.findAll();
     }
 
+    @GetMapping("/formPatient")
+    public String formPatient(Model model){
+        model.addAttribute("patient", new Patient() );
+        return "formPatient";
+    }
+
+    @PostMapping(path="/save")
+    public String save(Model model, @Valid Patient patient, BindingResult bindingResult,@RequestParam(defaultValue = "0")int page,@RequestParam(defaultValue = "") String keyword){
+        if(bindingResult.hasErrors()) return "formPatient";
+        patientRepository.save(patient);
+        return  "redirect:/index?page="+page+"&keyword="+keyword;
+    }
+
+    @GetMapping("/editPatient")
+    public String editPatient(Model model,Long id,String keyword, int page){
+        Patient patient=patientRepository.findById(id).orElse((null));
+        if (patient==null)
+            throw new RuntimeException("Patient  Introuvable" );
+        model.addAttribute("patient" ,patient);
+        model.addAttribute("page",page);
+        model.addAttribute("keyword",keyword);
+        return "editPatient";
+    }
 
 }
