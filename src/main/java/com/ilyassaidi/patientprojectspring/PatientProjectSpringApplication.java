60b4230ca09021847b9cce61bcce712778decbf1
Ilyass Aidi <74270120+ILYASS-DEV@users.noreply.github.com>
2022-04-24 17:29:20 +0100

    Edit bootstrap design

--- src/main/java/com/ilyassaidi/patientprojectspring/PatientProjectSpringApplication.java
+++ src/main/java/com/ilyassaidi/patientprojectspring/PatientProjectSpringApplication.java
@@ -15,17 +15,17 @@ public class PatientProjectSpringApplication {
     public static void main(String[] args) {
         SpringApplication.run(PatientProjectSpringApplication.class, args);
     }
-   /* @Bean
+    /*@Bean
     CommandLineRunner commandLineRunner(PatientRepository patientRepository){
         return args ->{
-            patientRepository.save(new Patient(null,"Ilyass",new Date(),false,102));
-            patientRepository.save(new Patient(null,"Youssef",new Date(),false,516));
-            patientRepository.save(new Patient(null,"Kawtar",new Date(),true,433));
-            patientRepository.save(new Patient(null,"Chaimaa",new Date(),false,10));
+            patientRepository.save(new Patient(null,"Ahmed",new Date(),false,102));
+            patientRepository.save(new Patient(null,"Zakaria",new Date(),false,516));
+            patientRepository.save(new Patient(null,"Ali",new Date(),true,433));
+            patientRepository.save(new Patient(null,"Najwa",new Date(),false,10));
             patientRepository.findAll().forEach(p->{
                 System.out.println(p.getNom());
             });
 
         };*/
 
-}
+} 
